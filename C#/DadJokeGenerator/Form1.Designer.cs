f7514896f4246a7706712075a9bfee68adade987
jld12 <jenndaniel94@gmail.com>
2018-01-16 17:44:45 -0500

    Dad Joke Generator - Form

---
+++ C#/DadJokeGenerator/Form1.Designer.cs
@@ -0,0 +1,93 @@
+﻿namespace Random
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.bindingSource1 = new System.Windows.Forms.BindingSource(this.components);
+            this.JokeButton = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.JokeLabel = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.bindingSource1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // JokeButton
+            // 
+            this.JokeButton.Location = new System.Drawing.Point(411, 12);
+            this.JokeButton.Name = "JokeButton";
+            this.JokeButton.Size = new System.Drawing.Size(156, 23);
+            this.JokeButton.TabIndex = 0;
+            this.JokeButton.Text = "Tell Me a Joke!";
+            this.JokeButton.UseVisualStyleBackColor = true;
+            this.JokeButton.Click += new System.EventHandler(this.JokeButton_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F);
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(207, 26);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Dad Joke Generator";
+            // 
+            // JokeLabel
+            // 
+            this.JokeLabel.AutoSize = true;
+            this.JokeLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F);
+            this.JokeLabel.Location = new System.Drawing.Point(14, 82);
+            this.JokeLabel.Name = "JokeLabel";
+            this.JokeLabel.Size = new System.Drawing.Size(15, 24);
+            this.JokeLabel.TabIndex = 2;
+            this.JokeLabel.Text = " ";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(579, 240);
+            this.Controls.Add(this.JokeLabel);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.JokeButton);
+            this.Name = "Form1";
+            this.Text = "Dad Joke Generator";
+            ((System.ComponentModel.ISupportInitialize)(this.bindingSource1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.BindingSource bindingSource1;
+        private System.Windows.Forms.Button JokeButton;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label JokeLabel;
+    }
+}
+
---
+++ C#/DadJokeGenerator/Form1.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Random
+{
+    public partial class Form1 : Form
+    {
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void JokeButton_Click(object sender, EventArgs e)
+        {
+            System.Random rand = new System.Random();
+            int randNum = rand.Next(20);
+
+            switch (randNum)
+            {
+                case 0:
+                    JokeLabel.Text = "Why can't you hear a pterodactyl go to the bathroom?"
+                        + "\nBecause the p is silent!";
+                    break;
+
+                case 1:
+                    JokeLabel.Text = "My ex-wife still misses me..."
+                        + "\nBut her aim is getting better!";
+                    break;
+
+                case 2:
+                    JokeLabel.Text = "Velcro..."
+                        + "\nWhat a ripoff!";
+                    break;
+
+                case 3:
+                    JokeLabel.Text = "Is pesticide good for crop-eating bugs?"
+                        + "\nNo--it kills them!";
+                    break;
+
+                case 4:
+                    JokeLabel.Text = "How do you find Will Smith in the snow?"
+                        + "\nLook for Fresh Prints!";
+                    break;
+
+                case 5:
+                    JokeLabel.Text = "Don't trust stairs..."
+                        + "\nThey're always up to something!";
+                    break;
+
+                case 6:
+                    JokeLabel.Text = "I love the ground."
+                        + "\nIt's always there for me when I'm down!";
+                    break;
+
+                case 7:
+                    JokeLabel.Text = "A group of mimes attacked a pedestrian..."
+                        + "\nTheir crimes were unspeakable!";
+                    break;
+
+                case 8:
+                    JokeLabel.Text = "What garb do men from Thailand wear to work?"
+                        + "\nA suit and Thai!";
+                    break;
+
+                case 9:
+                    JokeLabel.Text = "What do houses wear?"
+                        + "\nAddress!";
+                    break;
+
+                case 10:
+                    JokeLabel.Text = "A woman lost a marathon to a Sister..."
+                        + "\nBut she was second to nun!";
+                    break;
+
+                case 11:
+                    JokeLabel.Text = "Why did the scarecrow make the Dean's List?"
+                        + "\nIt was out standing in its field!";
+                    break;
+
+                case 12:
+                    JokeLabel.Text = "Why does a chicken coop have 2 doors?"
+                        + "\nIf it had 4, it would be a chicken sedan!";
+                    break;
+
+                case 13:
+                    JokeLabel.Text = "What happened when the priest blessed an avacado?"
+                        + "\nHe made guaca-holy!";
+                    break;
+
+                case 14:
+                    JokeLabel.Text = "I looked in the cupboard for some pita..."
+                        + "\nBut we had nhan!";
+                    break;
+
+                case 15:
+                    JokeLabel.Text = "I was going to watch the new Paddington movie..."
+                        + "\nBut I could bearly handle the first one!";
+                    break;
+
+                case 16:
+                    JokeLabel.Text = "I new scholarly article about a study involving fish came out..."
+                        + "\nIt's pier reviewed!";
+                    break;
+
+                case 17:
+                    JokeLabel.Text = "What's a good way to invest in bonds?"
+                        + "\nBuy a box set of all the movies!";
+                    break;
+
+                case 18:
+                    JokeLabel.Text = "Why do mermaids wear seashells?"
+                        + "\nThey can't fit in b-shells!";
+                    break;
+
+                case 19:
+                    JokeLabel.Text = "Why did the scientist compare the behavior of two beetles?"
+                        + "\nTo find the lesser of two weevils!";
+                    break;
+
+                default:
+                    JokeLabel.Text = "What do you call a knockoff sodoku puzzle?"
+                        + "\nPseudoku!";
+                    break;
+            }
+
+        }
+    }
+}
